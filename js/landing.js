613d18e823154e91a4121986b5c3f4e454a177f1
Apoorva <apoorva19.am@gmail.com>
2019-02-16 20:52:32 +0530

    New landing page

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,13 @@
 $('.flip').hover(function(){
     $(this).find('.card').toggleClass('flipped');
 });
+
+$(document).ready(function(){
+    $('.responsive').slick({
+        arrows: true,
+        dots: true,
+        autoplay: true, 
+        infinite: true,
+        speed: 6000,
+    });
+});
